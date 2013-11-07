d7f3efdac67a2220d7d2ab76962539c5655cd6c4
Bart Baryla <bartekdoe@o2.pl>
2013-11-07 20:57:56 +0100

    init

---
+++ app/views/microposts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @micropost, :content, :user_id, :comment, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c674cfbfe6c9a76d556d8f58494b370a8294d016da1a6828795ab674c91a6efe0a4e02c07587f2fee0a1e3196066bc320a27c22c9b30942fa3220e835bcaf984'
